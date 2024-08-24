341b63806fd3ec23f1f2fdcb0bcc626b5c2b680b
Julio Gomes Pena <juliogomespena@gmail.com>
2024-08-24 10:55:20 -0300

    Update indo além com propriedades get/set

--- Cadastro de clientes - simulação 01/Clientes.cs
+++ Cadastro de clientes - simulação 01/Clientes.cs
@@ -1,18 +1,69 @@
 ﻿class Cliente
 {
 
-    public string nome;
-    public long cpf;
-    public DateTime dataAniversario;
-    public string profissão;
-    public string comoConheceu;
-    public string endereco;
-    public long telefone;
-    public long celular;
-    public string email;
-    public string observacoes;
-    public DateTime dataInclusao = new DateTime();
+    public string Nome { get; set; }
+    public string Sobrenome { get; set; }
+    public long Cpf { get; set; }
+
+    public DateTime DataAniversario;
+    public string Profissao { get; set; }
+    public string ComoConheceu { get; set; }
+    public string Endereco { get; set; }
+    public long Telefone { get; set; }
+    public long Celular { get; set; }
+    public string Email { get; set; }
+    public string Observacoes { get; set; }
     public bool ClienteAtivo { get; set; }
+
+    //Modelo de escrita extensa:
+    public DateTime DataInclusao 
+    {
+
+        get
+        {
+
+            return DateTime.Now;
+
+        }
+    
+    }
+
+    //Modelo de escrita concisa:
+    public string NomeCompleto => $"{Nome} {Sobrenome}";
+
+
+
+    public void ExibirCadastroCliente()
+    {
+
+        Console.WriteLine($"Nome: {NomeCompleto}");
+        Console.WriteLine($"CPF: {Convert.ToUInt64(Cpf).ToString(@"000\.000\.000-00")}");
+        Console.WriteLine($"Aniversário: {DataAniversario.ToString("dd/MM/yyyy")}");
+        Console.WriteLine($"Profissão: {Profissao}");
+        Console.WriteLine($"Como nos conheceu: {ComoConheceu}");
+        Console.WriteLine($"Endereço: {Endereco}");
+        Console.WriteLine($"Telefone: {Convert.ToUInt64(Telefone).ToString("(00)0000-0000")}");
+        Console.WriteLine($"Celular: {Convert.ToUInt64(Celular).ToString("(00)00000-0000")}");
+        Console.WriteLine($"Email: {Email}");
+        Console.WriteLine($"Observações: {Observacoes}");
+        Console.WriteLine($"Data de inclusão: {DataInclusao}");
+
+        if(ClienteAtivo == true)
+        {
+
+            Console.WriteLine("Status: ativo\n\n");
+
+        }
+        else 
+        {
+
+            Console.WriteLine("Status: inativo\n\n");
+
+        }
+
+    }
+
+
 
     //se tornam desnecessários por conta das propriedades get set
     //public void RegistrarStatusCLiente(bool value)
@@ -28,36 +79,6 @@
     //    return clienteAtivo;
 
     //}
-
-    public void ExibirCadastroCliente()
-    {
-
-        Console.WriteLine($"Nome: {nome}");
-        Console.WriteLine($"CPF: {Convert.ToUInt64(cpf).ToString(@"000\.000\.000-00")}");
-        Console.WriteLine($"Aniversário: {dataAniversario.ToString("dd/MM/yyyy")}");
-        Console.WriteLine($"Profissão: {profissão}");
-        Console.WriteLine($"Como nos conheceu: {comoConheceu}");
-        Console.WriteLine($"Endereço: {endereco}");
-        Console.WriteLine($"Telefone: {Convert.ToUInt64(telefone).ToString("(00)0000-0000")}");
-        Console.WriteLine($"Celular: {Convert.ToUInt64(celular).ToString("(00)00000-0000")}");
-        Console.WriteLine($"Email: {email}");
-        Console.WriteLine($"Observações: {observacoes}");
-        Console.WriteLine($"Data de inclusão: {dataInclusao}");
-
-        if(ExibirStatusCliente())
-        {
-
-            Console.WriteLine("Status: ativo\n\n");
-
-        }
-        else 
-        {
-
-            Console.WriteLine("Status: inativo\n\n");
-
-        }
-
-    }
 
 }
 
--- Cadastro de clientes - simulação 01/Program.cs
+++ Cadastro de clientes - simulação 01/Program.cs
@@ -21,32 +21,32 @@ using System.Globalization;
 
 Cliente cliente01 = new();
 
-cliente01.nome = "Julio Gomes Pena";
-cliente01.cpf = 45898021809;
-DateTime.TryParseExact("02/09/1998", "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out cliente01.dataAniversario);
-cliente01.profissão = "Desenvolvedor C# NET.";
-cliente01.comoConheceu = "Anúncio no google.";
-cliente01.endereco = "Rua Hacib Kalaf, 255 - Jardim Paulista II - Jundiaí/SP";
-cliente01.telefone = 1145862516;
-cliente01.celular = 11981198884;
-cliente01.email = "juliogomespena@gmail.com";
-cliente01.observacoes = "Filho do Elias e da Valderez.";
-cliente01.dataInclusao = DateTime.Now;
+cliente01.Nome = "Julio";
+cliente01.Sobrenome = "Gomes Pena";
+cliente01.Cpf = 45898021809;
+DateTime.TryParseExact("02/09/1998", "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out cliente01.DataAniversario);
+cliente01.Profissao = "Desenvolvedor C# NET.";
+cliente01.ComoConheceu = "Anúncio no google.";
+cliente01.Endereco = "Rua Hacib Kalaf, 255 - Jardim Paulista II - Jundiaí/SP";
+cliente01.Telefone = 1145862516;
+cliente01.Celular = 11981198884;
+cliente01.Email = "juliogomespena@gmail.com";
+cliente01.Observacoes = "Filho do Elias e da Valderez.";
 cliente01.ClienteAtivo = true;
 
 Cliente cliente02 = new();
 
-cliente02.nome = "Lorena Antunes Rezende Pena";
-cliente02.cpf = 46125778415;
-DateTime.TryParseExact("26/01/2000", "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out cliente02.dataAniversario);
-cliente02.profissão = "Gestora de tráfego.";
-cliente02.comoConheceu = "Anúncio no instagram.";
-cliente02.endereco = "Rua Hacib Kalaf, 255 - Jardim Paulista II - Jundiaí/SP";
-cliente02.telefone = 1145862516;
-cliente02.celular = 11973695363;
-cliente02.email = "lorenarezende@gmail.com";
-cliente02.observacoes = "Filha do Josué e da Adriana.";
-cliente02.dataInclusao = DateTime.Now;
+cliente02.Nome = "Lorena";
+cliente02.Sobrenome = "Antunes Rezende Pena";
+cliente02.Cpf = 46125778415;
+DateTime.TryParseExact("26/01/2000", "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out cliente02.DataAniversario);
+cliente02.Profissao = "Gestora de tráfego.";
+cliente02.ComoConheceu = "Anúncio no instagram.";
+cliente02.Endereco = "Rua Hacib Kalaf, 255 - Jardim Paulista II - Jundiaí/SP";
+cliente02.Telefone = 1145862516;
+cliente02.Celular = 11973695363;
+cliente02.Email = "lorenarezende@gmail.com";
+cliente02.Observacoes = "Filha do Josué e da Adriana.";
 cliente02.ClienteAtivo = false;
 
 
